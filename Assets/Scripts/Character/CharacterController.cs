6a42069d6dc4f337d6bc91ecbabc4e62d95fdbf7
Kyle Cuss <108804713+Cussk@users.noreply.github.com>
2024-03-21 07:16:03 -0230

    character Data and Controller started

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,11 @@
+using Data;
+using UnityEngine;
+
+namespace Character
+{
+    public class CharacterController : MonoBehaviour
+    {
+        [SerializeField] CharacterData characterData;
+        
+    }
+}
---
+++ Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,39 @@
+
+using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(menuName = "CharacterData/ CharacterData")]
+    public class CharacterData : ScriptableObject
+    {
+        public string characterName;
+        
+        [Space(10)]
+        public int maxHealth;
+        public int currentHealth;
+        
+        [Space(10)]
+        public int maxMana;
+        public int currentMana;
+        
+        [Space(10)]
+        public int maxLimit;
+        public int currentLimit;
+        
+        [Space(10)]
+        public int maxSpeed;
+        public int currentSpeed;
+
+        [Space(10)] 
+        public CharacterState characterState;
+    }
+
+    public enum CharacterState
+    {
+        Loading,
+        Idle,
+        Ready,
+        Attacked,
+        Attacking
+    }
+}
